c3fd93985300ca0598ffd551c4139cf5e32241a5
hdb169 <hdb169@users.noreply.github.com>
2022-03-10 10:36:07 -0500

    Create setup.sh

---
+++ hcswif_swif2/setup.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/bash
+
+# -----------------------------------------------------------------------------
+#  Change these if this if not where hallc_replay and hcana live
+#export hcana_dir=/home/$USER/hcana
+#export hallc_replay_dir=/home/$USER/hallc_replay
+export hcana_dir=/u/group/c-csv/$USER/test_april2020_branch/hcana
+export hallc_replay_dir=/u/group/c-csv/$USER/test_april2020_branch/hallc_replay_sidis_fall18
+
+# -----------------------------------------------------------------------------
+#  Change if this gives you the wrong version of root, evio, etc
+#source /site/12gev_phys/softenv.sh 2.4
+source /apps/root/6.22.06/setroot_CUE.bash
+
+# -----------------------------------------------------------------------------
+# Source setup scripts
+curdir=`pwd`
+cd $hcana_dir
+source setup.sh
+export PATH=$hcana_dir/bin:$PATH
+echo Sourced $hcana_dir/setup.sh
+
+cd $hallc_replay_dir
+source setup.sh
+echo Sourced $hallc_replay_dir/setup.sh
+
+echo cd back to $curdir
+cd $curdir
+
